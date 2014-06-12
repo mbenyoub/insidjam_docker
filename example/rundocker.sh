47294ee4d8101244e71de7695080948aefd5d2e1
Florent Aide <florent.aide@gmail.com>
2014-06-12 11:56:45 +0200

    Removed -d flag since it is incompatible with --rm

--- example/rundocker.sh
+++ example/rundocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 8069:8069 --rm --name="xcgd.odoo" -v `pwd`/etc:/opt/openerp/etc -v `pwd`/other-addons:/opt/openerp/additionnal_addons -v `pwd`/var:/opt/openerp/var -v `pwd`/other-addons:/opt/openerp/additionnal_addons xcgd/odoo
+docker run -p 8069:8069 --rm --name="xcgd.odoo" -v `pwd`/etc:/opt/openerp/etc -v `pwd`/other-addons:/opt/openerp/additionnal_addons -v `pwd`/var:/opt/openerp/var -v `pwd`/other-addons:/opt/openerp/additionnal_addons xcgd/odoo
